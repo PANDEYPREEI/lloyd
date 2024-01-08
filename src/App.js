d8e0a8469e9013b7f9f392be16b93ba6634706ed
preetifoxmarlin <Preeti.pandey@foxmarlin.com>
2024-01-08 15:24:53 +0530

    annapurna-updated changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,244 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import lunch from "./lunchbg.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { motion, AnimatePresence } from "framer-motion"; // index.js or App.js or any other component file
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
+  let options = [];
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState("");
+  const [error, setError] = useState(null);
+  const carouse = [
+    "Cooking gone wrong ? ",
+    "Order your food",
+    "Delicious food",
+  ];
+  const marked = "Subramani";
+  const [isLucnhMark, setLunchMark] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [isAlreadyMarked, setAlreadyMarked] = useState(false);
+
+  const [currentCarouseIndex, setCurrentCarouseIndex] = useState(0);
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+
+  const todayFormatted = today.toISOString().split("T")[0]; // toISOString() method is used to convert the Date objects to a string in the ISO format
+  const tomorrowFormatted = tomorrow.toISOString().split("T")[0]; // "YYYY-MM-DDTHH:mm:ss.sssZ". split('T')[0] is then used to extract only the date part before the 'T', leaving us with "YYYY-MM-DD"
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleBack = () => {
+    setLunchMark(false);
+    setSelectedOption("");
+    setDate("");
+  };
+
+  const handleMarkLunch = async () => {
+    if (selectedOption == "") {
+      toast.error("Please select name");
+      return;
+    }
+    if (date == "") {
+      toast.error("Please select date");
+      return;
+    }
+    console.log(selectedOption, "selectedoption");
+    setLunchMark(true);
+    console.log(date, selectedOption, "date");
+
+    const req = {
+      date: date,
+      emp_code: selectedOption,
+    };
+    console.log(req);
+    try {
+      const apiUrl = "https://59.152.52.154:8180/data/mark";
+      const response = await axios.post(apiUrl, req);
+      console.log(response, response.data[0].sp_lunch.value);
+      const parsedValue = JSON.parse(response.data[0].sp_lunch.value);
+      console.log(parsedValue);
+      if (parsedValue.msg == "Lunch marked successfully.") {
+        setAlreadyMarked(false);
+        toast.success(parsedValue.msg, selectedOption);
+      } else if (parsedValue.msg == "Lunch is already marked!") {
+        setAlreadyMarked(true);
+        toast.warn(parsedValue.msg, selectedOption);
+      }
+    } catch (error) {
+      console.log(error, "error");
+      toast.error(error);
+      setError(error);
+    }
+  };
+
+  const handleDateChange = (event) => {
+    console.log(event.target.value, "date");
+    setDate(event.target.value);
+  };
+
+  useEffect(() => {
+    // Update the statement every 3 seconds
+    const intervalId = setInterval(() => {
+      setCurrentCarouseIndex((prevIndex) => (prevIndex + 1) % carouse.length);
+      console.log(currentCarouseIndex, carouse.length, currentCarouseIndex + 1);
+    }, 3000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [carouse.length]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = "https://59.152.52.154:8180/data";
+        const response = await axios.get(apiUrl);
+        console.log(response.data);
+        setData(response.data);
+        options = data;
+      } catch (error) {
+        console.log(error, "error");
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {/* Other components */}
+        <ToastContainer />
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <div className="lg:flex">
+            <div className="lg:w-1/2 p-12">
+              <div className="row mt-2 mb-3">
+                <p className="logo">Annapurna</p>
+              </div>
+              <div className="row mt-8">
+                <p className="carouse-text">{carouse[currentCarouseIndex]}</p>
+              </div>
+              <div className="row">
+                <p className="carouse-sub-text">
+                  Order food from your favourite restaurants near your place ?
+                </p>
+              </div>
+              <div>
+                {isLucnhMark ? (
+                  <>
+                    <div className="mt-10">
+                      <AnimatePresence>
+                        <motion.div
+                          initial={{ opacity: 0, scale: 0.8 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0.8 }}
+                          transition={{ duration: 0.5, ease: "easeInOut" }}
+                        >
+                          <div className="row mt-2 mb-3">
+                            <div className="d-flex col-md-12">
+                              <button className="back-btn" onClick={handleBack}>
+                                <i
+                                  className="mr-2 fa fa-arrow-left back-icon"
+                                  aria-hidden="true"
+                                ></i>
+                                <span className="back-text">Back</span>
+                              </button>
+                            </div>
+                          </div>
+                          <div className="marked-lunch-section">
+                            {!isAlreadyMarked ? (
+                              <div className="p-10">
+                                <i className="thumb-icon fas fa-thumbs-up fa-3x"></i>
+                                <p className="marked-lunch-text">
+                                  Lunch is suceessfully marked for{" "}
+                                  <span className="marked-text-color">
+                                    {" "}
+                                    {selectedOption}
+                                  </span>{" "}
+                                </p>
+                              </div>
+                            ) : (
+                              <div className="p-10">
+                                <i className="fa-regular thumb-icon fa-face-smile-wink fa-3x"></i>
+                                <p className="marked-lunch-text">
+                                  Lunch is already marked for
+                                  <span className="marked-text-color">
+                                    {" "}
+                                    {selectedOption}
+                                  </span>{" "}
+                                </p>
+                              </div>
+                            )}
+                          </div>
+                        </motion.div>
+                      </AnimatePresence>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="row mt-14">
+                      <div className="col-md-6 col-sm-12 col-xs-12">
+                        <input
+                          className="date-input form-control mb-3"
+                          placeholder="select date"
+                          type="date"
+                          onChange={handleDateChange}
+                          value={date}
+                          min={todayFormatted}
+                          max={tomorrowFormatted}
+                        />
+                      </div>
+                      <div className="col-md-6 col-sm-12 col-xs-12">
+                        <select
+                          className="dropdown form-control"
+                          value={selectedOption}
+                          onChange={handleSelectChange}
+                        >
+                          {/* Map through the options and create option elements */}
+                          <option key={"none"} value="">
+                            Select Name
+                          </option>
+                          {data.map((option) => (
+                            <>
+                              <option
+                                key={option.emp_code}
+                                value={option.emp_code}
+                              >
+                                {option.emp_fname} {option.emp_lname}
+                              </option>
+                            </>
+                          ))}
+                        </select>
+                      </div>
+                      <div className="col-md-12 d-flex justify-content-end mt-3">
+                        <button
+                          className="mark-lunch"
+                          onClick={handleMarkLunch}
+                        >
+                          Mark Lunch
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+
+            <div className="lg:w-1/2">
+              <img src={lunch} alt="lunch" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/options.ts
@@ -0,0 +1,362 @@
+export const options = [
+    {
+        "emp_code": "S00103",
+        "emp_fname": "Subramani",
+        "emp_mname": ".",
+        "emp_lname": "S"
+    },
+    {
+        "emp_code": "S00306",
+        "emp_fname": "Anirban",
+        "emp_mname": ".",
+        "emp_lname": "Halder"
+    },
+    {
+        "emp_code": "S00828",
+        "emp_fname": "Hemangi",
+        "emp_mname": "Swapnil",
+        "emp_lname": "Mahajan"
+    },
+    {
+        "emp_code": "S00958",
+        "emp_fname": "Nitesh",
+        "emp_mname": "Laxman",
+        "emp_lname": "Oza"
+    },
+    {
+        "emp_code": "S01012",
+        "emp_fname": "Sujeet",
+        "emp_mname": "Kumar",
+        "emp_lname": "Mandal"
+    },
+    {
+        "emp_code": "S01018",
+        "emp_fname": "Meenakshi",
+        "emp_mname": "V",
+        "emp_lname": "Iyer"
+    },
+    {
+        "emp_code": "S01032",
+        "emp_fname": "Santosh",
+        "emp_mname": "Raghunath",
+        "emp_lname": "Rowlo"
+    },
+    {
+        "emp_code": "S01061",
+        "emp_fname": "Yashwant",
+        "emp_mname": "Shantaram",
+        "emp_lname": "More"
+    },
+    {
+        "emp_code": "S01105",
+        "emp_fname": "Prasenjit",
+        "emp_mname": "Narayan",
+        "emp_lname": "Tayade"
+    },
+    {
+        "emp_code": "S01118",
+        "emp_fname": "Tejaswini",
+        "emp_mname": "Abasaheb",
+        "emp_lname": "Bhosale"
+    },
+    {
+        "emp_code": "S01123",
+        "emp_fname": "Harshad",
+        "emp_mname": "Ravikant",
+        "emp_lname": "Kadu"
+    },
+    {
+        "emp_code": "S01129",
+        "emp_fname": "Ujwala",
+        "emp_mname": "Bhaskar",
+        "emp_lname": "Salve"
+    },
+    {
+        "emp_code": "S01153",
+        "emp_fname": "Shiinu",
+        "emp_mname": "Thomas",
+        "emp_lname": "George"
+    },
+    {
+        "emp_code": "S01278",
+        "emp_fname": "Umesh",
+        "emp_mname": "Yashwant",
+        "emp_lname": "Kadam"
+    },
+    {
+        "emp_code": "S01391",
+        "emp_fname": "Naeemuddin",
+        "emp_mname": "Jamaluddin",
+        "emp_lname": "Ansari"
+    },
+    {
+        "emp_code": "S01436",
+        "emp_fname": "Subhasis",
+        "emp_mname": "Bhattacharjee",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S01532",
+        "emp_fname": "Suhas",
+        "emp_mname": "Dnyaneshwar",
+        "emp_lname": "Lande"
+    },
+    {
+        "emp_code": "S01578",
+        "emp_fname": "MohdYusuf",
+        "emp_mname": "Rukum",
+        "emp_lname": "Patel"
+    },
+    {
+        "emp_code": "S01622",
+        "emp_fname": "PRAFUL",
+        "emp_mname": "VISHRAM",
+        "emp_lname": "KARAMBELE"
+    },
+    {
+        "emp_code": "S01627",
+        "emp_fname": "SHEKHAR",
+        "emp_mname": "LAXMAN",
+        "emp_lname": "GADDAPAWAR"
+    },
+    {
+        "emp_code": "S01632",
+        "emp_fname": "PRAJAKTA",
+        "emp_mname": "TRIMBAK",
+        "emp_lname": "THUL"
+    },
+    {
+        "emp_code": "S01633",
+        "emp_fname": "BHARAT",
+        "emp_mname": "PATEL",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S01653",
+        "emp_fname": "AKSHAY",
+        "emp_mname": "RAJARAM",
+        "emp_lname": "JADHAV"
+    },
+    {
+        "emp_code": "S01674",
+        "emp_fname": "BIPIN",
+        "emp_mname": "NAMDEV",
+        "emp_lname": "PAWAR"
+    },
+    {
+        "emp_code": "S01709",
+        "emp_fname": "ANIL",
+        "emp_mname": "CHANAROO",
+        "emp_lname": "PAL"
+    },
+    {
+        "emp_code": "S01710",
+        "emp_fname": "SUMEET",
+        "emp_mname": "ANJARLEKAR",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S01746",
+        "emp_fname": "PRATAP",
+        "emp_mname": "PRAKASH",
+        "emp_lname": "Sahu"
+    },
+    {
+        "emp_code": "S01759",
+        "emp_fname": "MAHESH",
+        "emp_mname": "SATYARATH",
+        "emp_lname": "KAVATHEKAR"
+    },
+    {
+        "emp_code": "S01761",
+        "emp_fname": "SIDDHI",
+        "emp_mname": "Prakash",
+        "emp_lname": "Talawadekar"
+    },
+    {
+        "emp_code": "S01767",
+        "emp_fname": "MANI",
+        "emp_mname": "CHANDANA",
+        "emp_lname": "KOLLI"
+    },
+    {
+        "emp_code": "S01772",
+        "emp_fname": "PANKAJ",
+        "emp_mname": "SANTOSH",
+        "emp_lname": "AWASTHI"
+    },
+    {
+        "emp_code": "S01825",
+        "emp_fname": "DHANDAYUTHAPANI",
+        "emp_mname": "MANI",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S01835",
+        "emp_fname": "NIKHIL",
+        "emp_mname": "NATHURAM",
+        "emp_lname": "PAWAR"
+    },
+    {
+        "emp_code": "S01993",
+        "emp_fname": "JIGAR",
+        "emp_mname": "SHIROYA",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S02016",
+        "emp_fname": "VISHAL",
+        "emp_mname": "VIKRAM",
+        "emp_lname": "RAJGURU"
+    },
+    {
+        "emp_code": "S02069",
+        "emp_fname": "SUDHIR",
+        "emp_mname": "TIWARI",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S02156",
+        "emp_fname": "POOJAL",
+        "emp_mname": "BHALCHANDRA",
+        "emp_lname": "ANGRE"
+    },
+    {
+        "emp_code": "S02184",
+        "emp_fname": "PRIYANKA",
+        "emp_mname": "KSHIRSAGAR",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S02242",
+        "emp_fname": "DHANANJAY",
+        "emp_mname": "BHAGUJI",
+        "emp_lname": "YEWALE"
+    },
+    {
+        "emp_code": "S02247",
+        "emp_fname": "ALPESH",
+        "emp_mname": "SURESH",
+        "emp_lname": "MORE"
+    },
+    {
+        "emp_code": "S02279",
+        "emp_fname": "RAJESH",
+        "emp_mname": "LAXMAN",
+        "emp_lname": "SURVE"
+    },
+    {
+        "emp_code": "S02320",
+        "emp_fname": "SAANYA",
+        "emp_mname": "SUNIL",
+        "emp_lname": "KASBE"
+    },
+    {
+        "emp_code": "S02362",
+        "emp_fname": "SAGAR",
+        "emp_mname": "MONAPPA",
+        "emp_lname": "SUTAR"
+    },
+    {
+        "emp_code": "S02364",
+        "emp_fname": "AMIT",
+        "emp_mname": "RAULO",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S02377",
+        "emp_fname": "AKASH",
+        "emp_mname": "PRABHAKAR",
+        "emp_lname": "RAWLO"
+    },
+    {
+        "emp_code": "S02432",
+        "emp_fname": "JITENDRA",
+        "emp_mname": "MURLIDHAR",
+        "emp_lname": "WANI"
+    },
+    {
+        "emp_code": "S02437",
+        "emp_fname": "SAGAR",
+        "emp_mname": "DHARMA",
+        "emp_lname": "BACHHAV"
+    },
+    {
+        "emp_code": "S02443",
+        "emp_fname": "DEEPALI",
+        "emp_mname": "PRADEEP",
+        "emp_lname": "DUSANIS"
+    },
+    {
+        "emp_code": "S02450",
+        "emp_fname": "MUSUKURI",
+        "emp_mname": "SREENIVAS",
+        "emp_lname": "."
+    },
+    {
+        "emp_code": "S02458",
+        "emp_fname": "SHRIKANT",
+        "emp_mname": "DAMODARAN",
+        "emp_lname": "MUDALIAR"
+    },
+    {
+        "emp_code": "S02484",
+        "emp_fname": "VIJAYBABU",
+        "emp_mname": "NARAYANA",
+        "emp_lname": "PARGONDA"
+    },
+    {
+        "emp_code": "S02686",
+        "emp_fname": "ANANT",
+        "emp_mname": "VISHWANATH",
+        "emp_lname": "BANDWALKAR"
+    },
+    {
+        "emp_code": "S02697",
+        "emp_fname": "ANISH",
+        "emp_mname": "KANNIAH",
+        "emp_lname": "MUTHIAH"
+    },
+    {
+        "emp_code": "S02705",
+        "emp_fname": "SHIVA",
+        "emp_mname": ".",
+        "emp_lname": "MADDELA"
+    },
+    {
+        "emp_code": "S02709",
+        "emp_fname": "SANTOSHKUMAR",
+        "emp_mname": ".",
+        "emp_lname": "KAMMAM"
+    },
+    {
+        "emp_code": "S02710",
+        "emp_fname": "DINESH",
+        "emp_mname": ".",
+        "emp_lname": "DUBBA"
+    },
+    {
+        "emp_code": "S02712",
+        "emp_fname": "EDLINESHAMITHA",
+        "emp_mname": "EDWARD",
+        "emp_lname": "DSOUZA"
+    },
+    {
+        "emp_code": "S02718",
+        "emp_fname": "ASHWINI",
+        "emp_mname": "SURYAKANT",
+        "emp_lname": "GHADSHI"
+    },
+    {
+        "emp_code": "S02739",
+        "emp_fname": "SAIKIRANGOUD",
+        "emp_mname": ".",
+        "emp_lname": "RANGA"
+    },
+    {
+        "emp_code": "S02759",
+        "emp_fname": "AISHWARYA",
+        "emp_mname": "BIPIN",
+        "emp_lname": "DESHMANE"
+    }
+]
